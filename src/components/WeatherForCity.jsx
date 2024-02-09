a00e4af8bec350b7affd6bd83151d3d643011ed1
MichelaVivacqua <147068184+MichelaVivacqua@users.noreply.github.com>
2024-02-09 16:50:02 +0100

    completed

--- src/components/WeatherForCity.jsx
+++ src/components/WeatherForCity.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const WeatherForCity = ({ city }) => {
   const [weatherData, setWeatherData] = useState(null);
